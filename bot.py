c9714d0533446a41a2653e005445afa297f68b36
safarovd272-eng <safarovd272@gmail.com>
2026-03-24 22:50:39 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,34 @@
+import os
+import yt_dlp
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url = update.message.text
+
+    ydl_opts = {
+        'format': 'best',
+        'outtmpl': 'video.%(ext)s'
+    }
+
+    try:
+        await update.message.reply_text("⏳ Video yuklanmoqda...")
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            file = ydl.prepare_filename(info)
+
+        await update.message.reply_video(video=open(file, 'rb'))
+
+        os.remove(file)
+
+    except Exception as e:
+        await update.message.reply_text("❌ Yuklab bo‘lmadi.")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(MessageHandler(filters.TEXT, download))
+
+app.run_polling()
